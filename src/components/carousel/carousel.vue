e40ae35112fc4e3b15b5a4305e36ee24ea59778c
Lwj1378384589 <1378384589@qq.com>
2018-05-15 09:49:45 +0800

    项目

---
+++ src/components/carousel/carousel.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="fz-lunbo"> 
+        <!-- Hover触发-->
+        <div class="fz-lbbutton">
+            <div >
+                <el-carousel height="200px">
+                    <el-carousel-item  v-for="data in list " :key="data.src">
+                        <img v-bind:src="data.src">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default { 
+    data(){
+        return{
+            list:[
+                { src:'static/img/fz_carousel1.jpg' },
+                { src:'static/img/fz_carousel2.jpg' },
+                { src:'static/img/fz_carousel3.jpg' },
+                { src:'static/img/fz_carousel4.jpg' },
+                { src:'static/img/fz_carousel5.jpg' }
+            ]
+        }
+    }  
+}
+</script>
+<style >
+.fz-lunbo { float: right; height: 200px; width: 330px;border: #ddd 1px solid; }
+/* .fz-lunbo img{ height: 200px; width: 330px; } */
+.el-carousel__indicators{
+    left: 42%;
+}
+.el-carousel__button{
+    height: 4px;
+    border-radius: 3px;
+}
+.fz-lunbo img{
+     height: 200px; width: 330px;
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="index">
+      <my-header></my-header>
+      <!-- <my-aside></my-aside> -->
+      <router-view class="view"> </router-view>
+      <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+import MyHeader from '@/components/frame/header'
+import MyFooter from '@/components/frame/footer'
+import MyAside from '@/components/frame/aside'
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  },
+  components:{MyHeader,MyFooter,MyAside}
+}
+</script>
+
+<style scoped>
+  
+
+</style>
